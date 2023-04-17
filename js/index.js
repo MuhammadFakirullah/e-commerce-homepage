9ca0eeab1c33c2f2cad819a8d62e87ae3a72723f
Muhammad Fakirullah <88260844+MuhammadFakirullah@users.noreply.github.com>
2023-04-17 12:25:24 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+// Get the button
+var scrollBtn = document.getElementById("scrollBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+} else {
+    scrollBtn.style.display = "none";
+}
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollBtn.addEventListener("click", function() {
+window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+});
+});
+// Get the button element
+const cartBtn = document.getElementById("cart-btn");
+
+// Add an event listener to the button
+cartBtn.addEventListener("click", function() {
+  // Show an alert when the button is clicked
+  alert("Please login first before making any purchase");
+});
